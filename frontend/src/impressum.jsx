63ace3bca25a41836ece1fc732b4851e74949a12
Sebastian Weber <s-weber85@gmx.de>
2025-10-03 20:25:39 +0200

    Update SVG logos for improved design and consistency

---
+++ frontend/src/impressum.jsx
@@ -0,0 +1,44 @@
+
+
+
+function Impress() {
+
+  return (
+    <>
+    <html lang="en">
+    <body>
+      <div className="min-h-screen bg-slate-900 text-slate-300">
+        <div className="container mx-auto px-4 py-8">
+          <div className="flex flex-col min-h-screen">  
+
+              {/* -- 3. Tech stack icons --*/}
+              <div className="flex flex-col justify-center items-center px-4">
+                <div className="pt-2 flex flex-row justify-center ">
+
+
+
+                <br></br>
+                <div>
+                  <a href="/" className="underline text-slate-400 hover:text-slate-200">Home</a>
+                </div>
+                <div>Impress</div>
+                  <br></br>
+
+                  <br></br>
+                <div>©2025 Sebastian Weber</div>
+                  <br></br>
+              </div>
+            </div>
+            <br></br>
+        </div>
+        
+        </div>
+      </div>
+      </body>
+    </html>
+    </>
+
+  )
+}
+
+export default Impress
